eac59fc459fc916e0989e5e874461df72eb5beb9
Heather Jarvie <hjarvie@caringbridge.org>
2019-08-08 13:42:59 -0500

    built listen api, framework for other apis

--- webdecJS.js
+++ webdecJS.js
@@ -4,12 +4,22 @@ const searchURLpodcast = "https://listen-api.listennotes.com/api/v2/search";
 const wikipediaURL = "";
 const redditURL = "";
 
+//make Listen URL
+function createListenURL() {
+    const queryParams = Object.keys(params).map(function(key){ //go over during mentor session
+        const value = params[key]
+        return `${key}=${value}`
+    }).join("&")
+    const listenURL = `${searchURLpodcast}?${queryParams}`
+    return listenURL;
+};
+
 //manages user views
 function manageView() {
     $('.results-found').hide();
     $('.no-results-found').hide();
 
-    let resultsFound = 0; //TODO
+    let resultsFound = undefined;
 
     if (resultsFound === 0) {
         $('.no-results-found').show();
@@ -17,19 +27,17 @@ function manageView() {
         $('.results-found').show();
     };
 };
-//makes API request for Listen
-function makeListenAPIRequest() {
-    //TODO here's where I'll list out all the stuff to make my query
-}
 
-//makes API request for Wikipedia
-function makeWikiAPIRequest() {
-
-}
-
-//makes API Request for Reddit
-function makeRedditAPIRequest() {
-
+//compares search results to max allowed to determine what to display
+function manageResultsFound() {
+    const maxResults = $('#searchResultsCounts').val();
+    const actualResults = makeListenAPIRequest(resultsCount);
+    if (maxResults > actualResults) {
+        resultsFound = actualResults;
+    } else if (maxResults < actualResults) {
+        resultsFound = maxResults;
+    };
+    return resultsFound;
 }
 
 //listens for user input to send with API request
@@ -43,6 +51,46 @@ function submitUserInput() {
         makeRedditAPIRequest(caseName, resultsCount);
     });
 };
+
+//makes API request for Listen
+function makeListenAPIRequest(caseName, resultsCount) {
+    const listenQuery = {
+        q: caseName,
+        offset: resultsCount //TODO this might need to be changed since theirs returns 10 by default
+    };
+
+    const listenURL = createListenURL(listenQuery); 
+
+    fetch(listenURL, {
+        method: 'GET',
+        headers: {
+            'X-ListenAPI-Key': 'ff4a5a99e28e403181a8607ca2254322',
+            'App': 'WebDetective'
+        }
+    })
+        .then(function(response){
+            if (response.ok === true) {
+                return response.json()
+            }
+            throw new Error(response.statusText)
+        })
+        .then(function(responseJson){
+            console.log(responseJson)
+        })
+        .catch(function(error){
+            console.log("hey something broke", error)
+        })
+} 
+
+//makes API request for Wikipedia
+function makeWikiAPIRequest() {
+
+}
+
+//makes API Request for Reddit
+function makeRedditAPIRequest() {
+
+}
 
 //take all the functions and run it!
 function runPage() {
